ba58de0c51d03fb9a5b4cdec61713c1113992edc
schosio <schosio@protonmail.com>
2021-12-08 22:05:30 +0530

    trim update with multiple i/o

--- trim.sh
+++ trim.sh
@@ -3,10 +3,11 @@
 for infile in *R1.fastq.gz
 	do
 	echo $infile
-	outfile=$( echo $infile | sed 's/.fastq.gz/_trim.fastq.gz/')
 	name=$(echo $infile | awk -F"_R." '{print $1}')
-	echo $outfile
-	echo $name
-	echo "java -jar /home/chaos/softwares/Trimmomatic-0.39/trimmomatic-0.39.jar PE -version -threads 40 -phred33 -trimlog $outfile\trimLogFile.log -summary $outfile\statsSummaryFile.txt $infile ${name}_R2.fastq.gz $outfile ILLUMINACLIP:universal_adaptor.fa:2:40:15 AVGQUAL:24"
-    	java -jar /home/chaos/softwares/Trimmomatic-0.39/trimmomatic-0.39.jar PE -version -threads 40 -phred33 -trimlog $outfile\trimLogFile.log -summary $outfile\statsSummaryFile.txt $infile ${name}_R2.fastq.gz $outfile ILLUMINACLIP:universal_adaptor.fa:2:40:15 AVGQUAL:24
-	done
+	R1_paired=${name}_R1_paired.fastq.gz
+	R1_unpaired=${name}_R1_unpaired.fastq.gz
+	R2_paired=${name}_R2_paired.fastq.gz
+	R2_unpaired=${name}_R2_unpaired.fastq.gz
+	echo "java -jar /home/chaos/softwares/Trimmomatic-0.39/trimmomatic-0.39.jar PE -version -threads 40 -phred33 -trimlog ${name}_trimLogFile.log -summary ${name}_statsSummaryFile.txt $infile ${name}_R2.fastq.gz $R1_paired $R1_unpaired $R2_paired $R2_unpaired ILLUMINACLIP:universal_adaptor.fa:2:40:15 LEADING:28 TRAILING:28 AVGQUAL:28 MINLEN:100"
+	#java -jar /home/chaos/softwares/Trimmomatic-0.39/trimmomatic-0.39.jar PE -version -threads 40 -phred33 -trimlog ${name}_trimLogFile.log -summary ${name}_statsSummaryFile.txt $infile ${name}_R2.fastq.gz $R1_paired $R1_unpaired $R2_paired $R2_unpaired ILLUMINACLIP:universal_adaptor.fa:2:40:15 LEADING:28 TRAILING:28 AVGQUAL:28 MINLEN:100
+done
